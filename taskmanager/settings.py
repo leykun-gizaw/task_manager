d895613d80227829cd9da7bdeaeb2a67df0a9e7d
Leykun-Gizaw <Leykungizaw@gmail.com>
2025-09-15 18:15:08 +0300

    Add tasks app with initial configuration and files

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "tasks.apps.TasksConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
