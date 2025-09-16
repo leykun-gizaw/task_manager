f0d10f73459a35fb8f66d7a0a802e8b78ab6a70c
Leykun-Gizaw <Leykungizaw@gmail.com>
2025-09-16 10:08:19 +0300

    Implement task management features with models, views, and templates

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,8 @@
+from .models import Task
 from django.contrib import admin
 
+
 # Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "is_completed", "created_at")
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Task
+
 
 # Create your views here.
+def task_list(request):
+    tasks = Task.objects.all()
+    print(request)
+    return render(request, "tasks/task_list.html", {"tasks": tasks})
+
+
+def task_detail(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    print(request)
+    return render(request, "tasks/task_detail.html", {"task": task})
